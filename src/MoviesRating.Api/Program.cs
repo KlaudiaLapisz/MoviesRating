aa2cc56340db97fc752c91d9bcd2006665aec402
Klaudia Łapisz <lapisz.klaudia@gmail.com>
2024-04-29 11:00:02 +0200

    Added Serilog

--- src/MoviesRating.Api/Program.cs
+++ src/MoviesRating.Api/Program.cs
@@ -2,12 +2,20 @@ using MoviesRating.Application;
 using MoviesRating.Application.Commands;
 using MoviesRating.Domain;
 using MoviesRating.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDomain();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+{
+    loggerConfiguration.WriteTo
+        .Console()
+        .WriteTo
+        .File("logs.txt");
+});
 
 var app = builder.Build();
 app.UseInfrastructure();
--- src/MoviesRating.Infrastructure/Exceptions/ErrorHandlingMiddleware.cs
+++ src/MoviesRating.Infrastructure/Exceptions/ErrorHandlingMiddleware.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using MoviesRating.Domain.Exceptions;
 
 namespace MoviesRating.Infrastructure.Exceptions
 {
     internal class ErrorHandlingMiddleware : IMiddleware
     {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
@@ -13,6 +21,7 @@ namespace MoviesRating.Infrastructure.Exceptions
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, ex.Message);
                 if (ex is MovieRatingException)
                 {
                     context.Response.StatusCode = 400;
