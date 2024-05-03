0b09f33cc9d3c09302402005ba60fba3ae17ba65
Klaudia Łapisz <lapisz.klaudia@gmail.com>
2024-05-03 09:10:02 +0200

    Added behaviour to rate

--- src/MoviesRating.Domain/Entities/Rate.cs
+++ src/MoviesRating.Domain/Entities/Rate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesRating.Domain.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,15 +22,30 @@ namespace MoviesRating.Domain.Entities
         }
 
         public Rate (Guid id, int value, User user, Movie movie)
-        {           
-            Id = id;
-            Value = value;
+        {   
+            if(user == null)
+            {
+                throw new InvalidUserException();
+            }
+
+            if(movie == null)
+            {
+                throw new InvalidMovieException();
+            }           
+
+            Id = id;            
             User = user;
             Movie = movie;
+            UpdateValue(value);
         }
 
         public void UpdateValue(int value)
         {
+            if (value > 10 || value < 1)
+            {
+                throw new InvalidValueException();
+            }
+
             Value = value;
         }
     }
